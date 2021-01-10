e2b9557dfbfe88ef63d63dc40ec0c362d86c02dc
Evgeniy Panin <pane.ewgen@yandex.ru>
2021-01-10 16:02:21 +0400

    Внес данные о средствах

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,6 +1,6 @@
 export const config = {
     selectTitle: 'Выберите средство',
-    dateCommissioning: 'Дата ввода:',
+    dateCommissioning: 'Начало отсчета:',
     dateSelectorTitle: 'Выберите дату для подсчета наработки',
     resultText: ['Наработка на выбранную дату:', ' ч.']
 }
